b8444b467f967a590a6acc008969e8c928006c81
Rob Lim <robert.m.lim@gmail.com>
2025-05-23 00:55:18 +0000

    Work through 3.4

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/catkin_ws/src
+# Build directory: /home/user/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("my_package")
+subdirs("my_publisher_example_pkg")
+subdirs("simple_move_publisher")
---
+++ src/simple_move_publisher/scripts/simple_move_publisher.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist            
+
+rospy.init_node('SimpleMove')
+
+pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)    
+                                           
+rate = rospy.Rate(2)
+twist_msg = Twist()
+twist_msg.linear.x = 0.5
+# pub.publish(twist_msg)
+
+# Create a loop that will go until someone stops the program execution
+while not rospy.is_shutdown():
+  # Publish the message within the 'count' variable
+  pub.publish(twist_msg)
+  # Make sure the publish rate maintains at 2 Hz
+  rate.sleep()                             
